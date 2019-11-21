b321735cb6ab62cdbeaa239c4106726f3b7bd957
Gretelchen <gretelchen888@gmail.com>
2019-11-21 19:29:13 +0000

    add loeader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
+import Loader from "react-loader-spinner";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello World</p>
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+        />
       </header>
     </div>
   );
